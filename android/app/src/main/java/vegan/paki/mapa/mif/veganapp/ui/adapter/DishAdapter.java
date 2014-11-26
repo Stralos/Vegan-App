961c8e77d074e00d777ca5bb0f29d1fa6b8d438b
Paulius P <paulius.panda@gmail.com>
2014-11-26 23:09:37 +0200

    no message

--- android/app/src/main/java/vegan/paki/mapa/mif/veganapp/ui/adapter/DishAdapter.java
+++
@@ -1,7 +0,0 @@
-package vegan.paki.mapa.mif.veganapp.ui.adapter;
-
-/**
- * Created by Panda on 11/26/2014.
- */
-public class DishAdapter {
-}
--- android/app/src/main/java/vegan/paki/mapa/mif/veganapp/ui/fragment/DishFragment.java
+++ android/app/src/main/java/vegan/paki/mapa/mif/veganapp/ui/fragment/DishFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import vegan.paki.mapa.mif.veganapp.R;
 
@@ -13,9 +15,23 @@ import vegan.paki.mapa.mif.veganapp.R;
  */
 public class DishFragment extends Fragment {
 
+    private View initialiseView(View view){
+        TextView dishName = (TextView) view.findViewById(R.id.dish_name);
+        TextView dishDescription = (TextView) view.findViewById(R.id.how_to_dish);
+        ImageView imageView = (ImageView) view.findViewById(R.id.dish_image_view);
+
+        dishName.setText("SPAGETIOS!");
+        dishDescription.setText("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+
+
+        return view;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dish_view, container, false);
-        return view;
+
+        return initialiseView(view);
     }
 }
