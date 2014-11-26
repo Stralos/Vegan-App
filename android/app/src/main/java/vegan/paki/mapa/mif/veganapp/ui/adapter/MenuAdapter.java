2db87303775ba1b799a80f0536d9a19ab0a6af71
Mantas Palaima <palaima.mantas@gmail.com>
2014-11-26 23:07:29 +0200

    categories grid view layout

--- android/app/src/main/java/vegan/paki/mapa/mif/veganapp/ui/adapter/MenuAdapter.java
+++ android/app/src/main/java/vegan/paki/mapa/mif/veganapp/ui/adapter/MenuAdapter.java
@@ -1,14 +1,20 @@
 package vegan.paki.mapa.mif.veganapp.ui.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseFile;
 
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import vegan.paki.mapa.mif.veganapp.R;
 import vegan.paki.mapa.mif.veganapp.core.model.dto.CategoryDTO;
 import vegan.paki.mapa.mif.veganapp.util.RxImageLoader;
 
@@ -27,24 +33,39 @@ public class MenuAdapter extends BaseAdapter {
 
     public int getCount(){return categories.size();}
 
-    public Object getItem(int position){return categories.get(position);}
+    public CategoryDTO getItem(int position){return categories.get(position);}
 
     public long getItemId(int position){return 0;}
 
     public View getView(int position, View convertView, ViewGroup parent){
-        ImageView iv;
-        if(convertView != null){
-            iv = (ImageView) convertView;
-        }else{
-            iv = new ImageView(mContext);
-            iv.setLayoutParams(new GridView.LayoutParams(300,300));
-            //iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            //iv.setPadding(8,8,8,8);
+        View view = convertView;
+        final ViewHolder holder;
+        if (view != null) {
+            holder = (ViewHolder) view.getTag();
+        } else {
+            view = LayoutInflater.from(mContext).inflate(R.layout.list_item_category, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        }
+        CategoryDTO categoryDTO = getItem(position);
+        holder.mTitle.setText(categoryDTO.getName());
+        ParseFile image = categoryDTO.getImage();
+        if (image != null) {
+            RxImageLoader.displayImage(image.getUrl(), holder.mImage).cache().subscribe();
+        } else {
+            RxImageLoader.displayImage("drawable://"+R.drawable.vegan_placeholder, holder.mImage).cache().subscribe();
         }
 
-        RxImageLoader.displayImage(categories.get(position).getImage().getUrl(), iv).cache().subscribe();
-        //iv.setImageResource(R.drawable.vegan_placeholder);
-        return iv;
+        return view;
+    }
+
+    static class ViewHolder {
+        @InjectView(R.id.title) TextView mTitle;
+        @InjectView(R.id.image) ImageView mImage;
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
     }
 
 
